9bbc48dd6d87ce25c35d1d7a5667f2091cc129ea
andrei <talpanandrei@gmail.com>
2023-03-13 21:09:25 +0000

    removed model tiny face

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
